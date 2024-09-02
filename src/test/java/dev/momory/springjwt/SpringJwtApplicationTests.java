9073f387b1ff2d46a013896d05590b1bf88324fe
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-09-02 11:23:23 +0900

    Initial commit

---
+++ src/test/java/dev/momory/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.momory.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

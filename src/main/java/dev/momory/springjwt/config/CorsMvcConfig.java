96b7b78fca4c785c73a4b39794db4f233915499e
memoryDev <115910144+memoryDev@users.noreply.github.com>
2024-09-02 16:52:30 +0900

    CORS 설정 추가 완료

---
+++ src/main/java/dev/momory/springjwt/config/CorsMvcConfig.java
@@ -0,0 +1,13 @@
+package dev.momory.springjwt.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173");
+    }
+}
--- src/main/java/dev/momory/springjwt/controller/AdminController.java
+++ src/main/java/dev/momory/springjwt/controller/AdminController.java
@@ -1,13 +1,34 @@
 package dev.momory.springjwt.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 @RestController
+@Slf4j
 public class AdminController {
 
     @GetMapping("/admin")
     public String adminP() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+
+        log.info("username={}", username);
+        log.info("role={}", role);
+
         return "admin Controller";
     }
 }
